31373a4efa902a6778d102512e5c24e3e5f573bd
James Jones <jimmy.jones@tutanota.com>
2020-01-29 15:33:01 -0500

    add rate limiting

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 const cors = require('cors')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const userRouter = require('./routes/userRoutes')
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP. Please try again in 1 hour.'
+})
+
 const app = express()
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use('/api', limiter)
 app.use(
   cors({
     origin: 'http://localhost:3000',
     credentials: true
   })
 )
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
 app.use(express.json())
 app.use((req, res, next) => {
   console.log('request time: ', new Date().toISOString())
