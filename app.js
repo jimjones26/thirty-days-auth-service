4b93a1652731639b3db34cb7d58b34223e4f211d
James Jones <jimmy.jones@tutanota.com>
2020-01-29 19:26:43 -0500

    add xss injection attack protection middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const xss = require('xss-clean')
 const cors = require('cors')
 
 const AppError = require('./utils/appError')
@@ -20,14 +21,22 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(helmet())
+app.use(xss())
 app.use(
   cors({
     origin: 'http://localhost:3000',
     credentials: true
   })
 )
+
+// limit the amount of requests an ip can make in a time
 app.use(limiter)
+
+// limit size of json payload
 app.use(express.json({ limit: '10kb' }))
+
+// data sanitization against XSS
+app.use(xss())
 app.use((req, res, next) => {
   console.log('request time: ', new Date().toISOString())
 
