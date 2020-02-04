d8b1b7a9e341bef83641a053c02d3a62c0b9130e
James Jones <jimmy.jones@tutanota.com>
2020-02-04 15:58:38 -0500

    add properly formed email check to login

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,13 @@ const { transporter, magicLinkEmailTemplate } = require('./../utils/email')
 exports.login = catchAsync(async (req, res, next) => {
   // TODO: how to make sure email is well formed?
   const { email } = req.body
-  if (!email) {
+  if (
+    !email ||
+    (email &&
+      !email.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      ))
+  ) {
     return res.status(400).json({
       status: 'error',
       message: 'an email is required'
