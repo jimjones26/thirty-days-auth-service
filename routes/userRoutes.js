db6b66ce12f2710dc0b6bc3b025278b61fb4f0f1
James Jones <jjones@Jamess-MacBook-Pro.local>
2020-05-15 08:03:27 -0400

    remove unused code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,11 @@
-const express = require('express')
-const userController = require('./../controllers/userController')
-const authController = require('./../controllers/authController')
+const express = require("express");
+const authController = require("./../controllers/authController");
 
-const router = express.Router()
+const router = express.Router();
 
-/* router.param('id', userController.checkID) */
-router.post('/login', authController.login)
-router.post('/create-tokens', authController.create_tokens)
-router.post('/refresh-tokens', authController.refresh_tokens)
-router.post('/revoke-tokens', authController.revoke_tokens)
+router.post("/login", authController.login);
+router.post("/create-tokens", authController.create_tokens);
+router.post("/refresh-tokens", authController.refresh_tokens);
+router.post("/revoke-tokens", authController.revoke_tokens);
 
-router.route('/').post(userController.createUser)
-
-router.route('/:id').get(userController.getUserById)
-
-module.exports = router
+module.exports = router;
