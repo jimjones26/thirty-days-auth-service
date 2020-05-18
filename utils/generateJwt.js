0498b9a7b566a828ee77f4e8a18e8f002239cc13
James Jones <jjones@Jamess-MacBook-Pro.local>
2020-05-17 22:06:19 -0400

    change majic link token expirery

--- utils/generateJwt.js
+++ utils/generateJwt.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 module.exports.createMagicLinkToken = (id, email) => {
   return jwt.sign({ id, email }, process.env.MAGIC_LINK_TOKEN_SECRET, {
-    expiresIn: '1hr'
-  })
-}
+    expiresIn: "15m",
+  });
+};
 
 module.exports.createRefreshToken = (id, tokenVersion) => {
   return jwt.sign({ id, tokenVersion }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: '7d'
-  })
-}
+    expiresIn: "7d",
+  });
+};
 
 module.exports.createAccessToken = (id, email, firstName, lastName) => {
   return jwt.sign(
     { id, email, firstName, lastName },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: '15m'
+      expiresIn: "15m",
     }
-  )
-}
+  );
+};
